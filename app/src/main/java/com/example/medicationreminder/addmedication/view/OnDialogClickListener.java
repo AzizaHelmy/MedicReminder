e4925957c280e7bb84895ce1f81c665ca42e8ba2
SalmaElawady <salmaelawady246@gmail.com>
2022-02-25 13:44:26 +0200

    adding listener class

---
+++ app/src/main/java/com/example/medicationreminder/addmedication/view/OnDialogClickListener.java
@@ -0,0 +1,8 @@
+package com.example.medicationreminder.addmedication.view;
+
+public interface OnDialogClickListener {
+    public void displayText(String number);
+    public void showText(String number);
+    public void chooseRefillAmount(String amount);
+    public void displayNumbreTwo(String number);
+}
